d105e3c15daaac745539bdd3e4a1f743b8feda09
Tyler Treat <ttreat31@gmail.com>
2017-08-17 13:50:59 -0500

    Don't hardcode scaling

--- writer.go
+++ writer.go
@@ -14,8 +14,8 @@ import (
 // format plottable by http://hdrhistogram.github.io/HdrHistogram/plotFiles.html
 // to the given Writer. Percentiles is a list of percentiles to include, e.g.
 // 10.0, 50.0, 99.0, 99.99, etc. If percentiles is nil, it defaults to a
-// logarithmic percentile scale.
-func WriteDistribution(hist *hdrhistogram.Histogram, percentiles Percentiles, writer io.Writer) error {
+// logarithmic percentile scale. The scaleFactor is used to scale values.
+func WriteDistribution(hist *hdrhistogram.Histogram, percentiles Percentiles, scaleFactor float64, writer io.Writer) error {
 	if percentiles == nil {
 		percentiles = Logarithmic
 	}
@@ -26,7 +26,7 @@ func WriteDistribution(hist *hdrhistogram.Histogram, percentiles Percentiles, wr
 
 	totalCount := hist.TotalCount()
 	for _, percentile := range percentiles {
-		value := float64(hist.ValueAtQuantile(percentile)) / 1000000
+		value := float64(hist.ValueAtQuantile(percentile)) * scaleFactor
 		oneByPercentile := getOneByPercentile(percentile)
 		countAtPercentile := int64(((percentile / 100) * float64(totalCount)) + 0.5)
 		_, err := writer.Write([]byte(fmt.Sprintf("%f    %f        %d            %f\n",
@@ -43,8 +43,9 @@ func WriteDistribution(hist *hdrhistogram.Histogram, percentiles Percentiles, wr
 // format plottable by http://hdrhistogram.github.io/HdrHistogram/plotFiles.html
 // to the given file. If the file doesn't exist, it's created. Percentiles is a
 // list of percentiles to include, e.g.  10.0, 50.0, 99.0, 99.99, etc. If
-// percentiles is nil, it defaults to a logarithmic percentile scale.
-func WriteDistributionFile(hist *hdrhistogram.Histogram, percentiles Percentiles, file string) error {
+// percentiles is nil, it defaults to a logarithmic percentile scale. The
+// scaleFactor is used to scale values.
+func WriteDistributionFile(hist *hdrhistogram.Histogram, percentiles Percentiles, scaleFactor float64, file string) error {
 	f, err := os.Open(file)
 	if err != nil {
 		f, err = os.Create(file)
@@ -54,7 +55,7 @@ func WriteDistributionFile(hist *hdrhistogram.Histogram, percentiles Percentiles
 	}
 	defer f.Close()
 
-	return WriteDistribution(hist, percentiles, f)
+	return WriteDistribution(hist, percentiles, scaleFactor, f)
 }
 
 func getOneByPercentile(percentile float64) float64 {
